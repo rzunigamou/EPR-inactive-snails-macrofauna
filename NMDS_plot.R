af946a972b3871f1c19a157044b711ada69bda75
rzunigamou <167127483+rzunigamou@users.noreply.github.com>
2024-10-04 17:18:01 -0400

    Update NMDS_plot.R
    Includes ggplot code and ensures presence/absence jaccard distance data

--- NMDS_plot.R
+++ NMDS_plot.R
@@ -15,6 +15,8 @@
 library(dplyr)
 library(data.table)
 library(vegan)
+library(stringr)
+library(ggplot2)
 
 #input <- read.csv("Gastropod_Counts_Assembled_At_sea_20240926_edited.csv") - OLD SPREADSHEET
 input <- read.csv("supp_Table_Snail_ms.csv")
@@ -31,16 +33,16 @@ data[1,] <- gsub("\\*","",data[1,])
 
 # append rock type to column names and remove rocktype row
 new_col_names <- paste (colnames(data), data[1,], sep = "_")
-colnames(data) <- new_col_names
-data <- data[-1,]
+data_conc <- data[-1,]
+colnames(data_conc) <- new_col_names
 
 
-t_data <- transpose(data)
-rownames(t_data) <- colnames(data)
+t_data <- transpose(data_conc)
+rownames(t_data) <- colnames(data_conc)
 colnames(t_data) <- t_data[1,]
 t_data <- t_data[-1, ]
 
-
+# data for vegan NMDS -----------------
 
 data_for_vegan <- as.data.frame(sapply(t_data, as.numeric))
 rownames(data_for_vegan) <- rownames(t_data)
@@ -48,11 +50,83 @@ rownames(data_for_vegan) <- rownames(t_data)
 #remove empty colums
 data_for_vegan <- data_for_vegan[, colSums(is.na(data_for_vegan)) < nrow(data_for_vegan)]
 
-
 #note we need to edit row names for better plot below
 #because R adds .1, .2, etc upon subsequent same rowname
 
-set.seed(50)
-data_nmds <- metaMDS(data_for_vegan, distance = "jaccard", autotransform = FALSE, k=2, trymax=100)
-#plot(data_nmds, type="t")
+#Set NMDS data, assuring binary (presence/absence) jaccard distance
+set.seed(50) #arbitrary value for random number generato
+jacc_dist <- vegdist(data_for_vegan, method = "jaccard", binary = T)
+data_nmds <- metaMDS(jacc_dist, autotransform = FALSE)
 plot(data_nmds, display = "sites", type="t")
+
+
+# --------------------------Plotting in ggplot ----------
+# pull out sites and color
+ Rock_color <- as.character(data[1,-1])
+ sites <- colnames(data[,-1])
+ 
+# make a dataframe with the nmds points and site data
+
+EFAplot <- data.frame(NMDS1 = data_nmds$points[,1], NMDS2 = data_nmds$points[,2], Site_OG = sites, RockType = Rock_color)
+# add abbreviated site factors
+EFAplot <- EFAplot |> 
+  mutate(Site = case_when(
+    str_detect(Site_OG, "Sentry") ~ "Sentry Spire",
+    str_detect(Site_OG, "Lucky") ~ "Lucky's Mound"
+  ))
+EFAplot$Site <- factor(EFAplot$Site)
+EFAplot$RockType <- factor(EFAplot$RockType)
+EFAplot.mean=aggregate(EFAplot[,1:2],list(group = EFAplot$Site), mean)
+
+#  ELLIPSES
+# veganCovEllipse<-function(cov, center = c(0, 0), scale = 1, npoints = 100) 
+# {
+#   theta <- (0:npoints) * 2 * pi/npoints
+#   Circle <- cbind(cos(theta), sin(theta))
+#   t(center + scale * t(Circle %*% chol(cov)))
+# }
+# 
+# df_ell <- data.frame()
+# for(g in levels(EFAplot$Site)){
+#   df_ell <- rbind(df_ell, cbind(as.data.frame(with(EFAplot[EFAplot$Site==g,],
+#                                                    veganCovEllipse(cov.wt(cbind(NMDS1,NMDS2),wt=rep(1/length(MDS1),length(MDS1)))$cov,center=c(mean(MDS1),mean(MDS2)))))
+#                                 ,Site=g))
+# }
+
+ggplot(data = EFAplot, aes(x = NMDS1, y = NMDS2)) + 
+  geom_point(aes(shape = Site, color = RockType), size = 5) + xlim(-1.25,1.25) + ylim(-1.25,1.25) +
+#  geom_path(data=df_ell, aes(x=MDS1, y=MDS2, linetype = Site), size=0.6)+ #ELLIPSES
+#  scale_linetype_manual(values= c("dashed", "dotted"))+ #ELLIPSES
+  scale_color_manual(values = c("brown", "aquamarine2", "orange2")) +
+  theme_bw() + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(panel.border = element_rect(fill=NA, colour="black", size = 1)) +
+  theme(axis.text = element_text(colour="black", size = 12),
+        axis.title = element_text(colour="black", size = 14))
+#  guides(linetype = guide_legend(order=1), shape = guide_legend(order =2)) #ELLIPSES
+
+# GGSAVE
+
+#  "nmds_fauna_Fig5b_2024-07-17.tif",
+#   "nmds_fauna_Fig4b_2024-06-28.tif",
+  ggsave(
+  filename = 'inactiveNMDS.tiff',
+  plot = last_plot(),
+  device = NULL,
+  path = "C:\\Users\\Rodrigo Zuniga\\Documents\\github\\EPR-inactive-snails\\", # output files in separate folder
+  scale = 1,
+  width = NA,
+  height = NA,
+  units = c("in", "cm", "mm", "px"),
+  dpi = 500,
+  limitsize = TRUE,
+  bg = NULL,
+)
+
+
+
+
+
+
+
+
