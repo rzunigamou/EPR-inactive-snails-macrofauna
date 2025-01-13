1ec8ddb7579d0068968f27197654ec3cb2538acd
Stace Beaulieu <stace@whoi.edu>
2025-01-13 16:50:18 -0500

    added Features in ggplot and saved TIF

--- NMDS_plot_Merged_Per&Sul.R
+++ NMDS_plot_Merged_Per&Sul.R
@@ -5,11 +5,16 @@
 library(readxl)
 library(data.table) # for transpose function
 library(vegan)
+library(dplyr)
+library(stringr)
+library(ggplot2)
 
-# will need to retain metadata from top rows
+# will need top row "Feature/Site" for ggplot
+input_top <- read_excel("C:/Users/sbeaulieu/Downloads/Periphery_EcolMon2003_Block&Sieve.xlsx", sheet = "Merged_Per&Sul", skip = 1)
+input_sites <- colnames(input_top[,-1])
+
 # but starting with removing top rows to quickly plot
 # samples get auto-numbered 2 to 25
-# will need to add row for rock color
 
 input_new <- read_excel("C:/Users/sbeaulieu/Downloads/Periphery_EcolMon2003_Block&Sieve.xlsx", sheet = "Merged_Per&Sul", skip = 6)
 
@@ -29,4 +34,48 @@ set.seed(50) #arbitrary value for random number generator
 jacc_dist <- vegdist(data_for_vegan, method = "jaccard", binary = T)
 data_nmds <- metaMDS(jacc_dist, autotransform = FALSE)
 plot(data_nmds, display = "sites", type="t")
+
+# --------------------------Plotting in ggplot ----------
+# make a dataframe with the nmds points and site data
+EFAplot <- data.frame(NMDS1 = data_nmds$points[,1], NMDS2 = data_nmds$points[,2], Site_OG = input_sites)
+# add abbreviated site factors
+EFAplot <- EFAplot |> 
+  mutate(Feature = case_when(
+    str_detect(Site_OG, "Sentry") ~ "Sentry Spire",
+    str_detect(Site_OG, "Lucky") ~ "Lucky's Mound",
+    str_detect(Site_OG, "EW") ~ "East Wall",
+    str_detect(Site_OG, "BV") ~ "Biovent",
+    str_detect(Site_OG, "WH") ~ "Worm Hole"
+  ))
+EFAplot$Feature <- factor(EFAplot$Feature)
+
+# need Lucky's Mound shape 16 filled circle
+# need Sentry Spire shape 17 filled triangle point-up
+ggplot(data = EFAplot, aes(x = NMDS1, y = NMDS2)) + 
+  geom_point(aes(shape = Feature), size = 4) + xlim(-2.4,2.4) + ylim(-2.4,2.4) +
+  coord_fixed()+
+  scale_shape_manual(values = c(5, 8, 16, 17, 6)) +
+  theme_bw() + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+  theme(panel.border = element_rect(fill=NA, colour="black", size = 1)) +
+  theme(axis.text = element_text(colour="black", size = 12),
+        axis.title = element_text(colour="black", size = 14))+
+  guides(color = guide_legend(override.aes = list(shape = 15, size = 4)))
+
+# GGSAVE
+
+ggsave(
+  filename = 'NMDS_Merged_Per&Sul_20250113.tiff',
+  plot = last_plot(),
+  device = NULL,
+  path = "C:\\Users\\sbeaulieu\\Desktop\\", # output files in separate folder
+  scale = 1,
+  width = NA,
+  height = NA,
+  units = c("in", "cm", "mm", "px"),
+  dpi = 500,
+  limitsize = TRUE,
+  bg = NULL,
+)
+
 
